6cec2ecff5de7b25a754e25cfdb2d60d8565e481
Paloma Regis <palomaregis29@gmail.com>
2021-11-16 22:06:14 -0300

    incluindo funcao para inserir tarefa

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ToastController } from '@ionic/angular';
+import { toastController } from '@ionic/core';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,8 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor(private alertCtrl : AlertController) {}
+  tasks : any[] = [];//criando objeto vazio
+  constructor(private alertCtrl : AlertController, private toastCtrl : ToastController) {}
 
   async showAdd(){
     const alert = await this.alertCtrl.create({
@@ -32,11 +33,33 @@ export class HomePage {
         {
           text: 'Adiocionar',
           handler: (form) => {
-           // this.add(form.taskToDo);
+          this.add(form.taskToDo);
           }
         }
       ]
     });
     await alert.present();
+  }
+
+  async add(taskToDo : string){
+    //valida se o usuario preencheu a tarefa
+    if(taskToDo.trim().length < 1){
+      const toast = await this.toastCtrl.create({
+        message : 'Informe o que deseja fazer',
+        duration: 2000,
+        position : 'top'
+      });
+
+      toast.present();
+      return;
+    }
+
+    let task = {name: taskToDo, done: false};
+    this.tasks.push(task);
+    this.updateLocalStorage();
+  }
+
+  updateLocalStorage(){
+    localStorage.setItem('taskdb', JSON.stringify(this.tasks));
   }
 }
