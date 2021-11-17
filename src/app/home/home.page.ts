7c03643bbd64db18ca5c7aac26fc8e29a4e67544
Paloma Regis <palomaregis29@gmail.com>
2021-11-17 16:36:03 -0300

    armazenamento localStorage e status task

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AlertController, ToastController } from '@ionic/angular';
+import { ActionSheetController, AlertController, ToastController } from '@ionic/angular';
 import { toastController } from '@ionic/core';
 
 @Component({
@@ -8,9 +8,13 @@ import { toastController } from '@ionic/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  tasks : any[] = [];//criando objeto vazio
-  constructor(private alertCtrl : AlertController, private toastCtrl : ToastController) {}
-
+  tasks: any[] = [];//criando objeto vazio
+  constructor(private alertCtrl: AlertController, private toastCtrl: ToastController, private actionSheetCtrl: ActionSheetController) {
+    let taskJson = localStorage.getItem('taskdb');//armazenando apos carregar
+    if(taskJson!=null){
+      this.tasks = JSON.parse(taskJson);
+    }
+  }//fim do bloco de armazenamento
   async showAdd(){
     const alert = await this.alertCtrl.create({
       header: 'O que deseja fazer?',
@@ -27,7 +31,7 @@ export class HomePage {
           role: 'cancel',
           cssClass: 'secondary',
           handler: () => {
-            console.log('clicked cancel')
+            console.log('clicked cancel');
           }
         },
         {
@@ -41,7 +45,7 @@ export class HomePage {
     await alert.present();
   }
 
-  async add(taskToDo : string){
+  async add(taskToDo: string){
     //valida se o usuario preencheu a tarefa
     if(taskToDo.trim().length < 1){
       const toast = await this.toastCtrl.create({
@@ -54,12 +58,35 @@ export class HomePage {
       return;
     }
 
-    let task = {name: taskToDo, done: false};
+    let task = {name: taskToDo, done: false};//insere a tarefa na lista
     this.tasks.push(task);
     this.updateLocalStorage();
   }
 
   updateLocalStorage(){
     localStorage.setItem('taskdb', JSON.stringify(this.tasks));
+  }//fim da inserção
+
+  async openActions(task: any){
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: "O QUE DESEJA FAZER?",
+      buttons: [{
+        text: task.done? 'Desmarcar' : 'Marcar',
+        icon: task.done? 'radio-button-off' : 'checkmark-circle',
+        handler: () => {
+          task.done = !task.done;
+          this.updateLocalStorage();
+        }
+      },
+       {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
   }
 }
